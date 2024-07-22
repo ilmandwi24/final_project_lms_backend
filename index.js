zhafranafif/zhafran-RTB-AnimeApi

Nabhan | initial commit

index.js
@@ -1,0 +1,48 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const Boom = require('boom');
+
+const CommonHelper = require('./server/helpers/CommonHelper');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Import Route
+const country = require('./server/api/country');
+
+// Middleware
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  const oldSend = res.send;
+  res.send = async (data) => {
+    res.send = oldSend; // set function back to avoid the 'double-send'
+    const response = await CommonHelper.unifyResponse(req, res, data);
+
+    // Log Transaction
+    const logData = CommonHelper.logRequest(req, response);
+
+    CommonHelper.log(['API Request', 'info'], logData);
+    return res.status(response.statusCode).send(response.bodyResponse); // just call as normal with data
+  };
+
+  next();
+});
+
+// Route middlewares
+app.use('/country', country);
+
+app.get('/sys/ping', (req, res) => {
+  req.startTime = process.hrtime();
+  res.send('ok');
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send(Boom.notFound('No route matched with those values', {}));
+});
+
+app.listen(port, () => {
+  CommonHelper.log(['Info'], `Server started on port ${port}`);
+});

ilmandwi24/final_project_lms_backend

index.js
@@ -1,0 +1,48 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const Boom = require('boom');
+
+const CommonHelper = require('./server/helpers/CommonHelper');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// Import Route
+const sample = require('./server/api/sample');
+
+// Middleware
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  const oldSend = res.send;
+  res.send = async (data) => {
+    res.send = oldSend; // set function back to avoid the 'double-send'
+    const response = CommonHelper.unifyResponse(req, res, data);
+
+    // Log Transaction
+    const logData = CommonHelper.logRequest(req, response);
+
+    CommonHelper.log(['API Request', 'info'], logData);
+    return res.status(response.statusCode).send(response.bodyResponse); // just call as normal with data
+  };
+
+  next();
+});
+
+// Route middlewares
+app.use('/api/sample', sample);
+
+app.get('/sys/ping', (req, res) => {
+  req.startTime = process.hrtime();
+  res.send('ok');
+});
+
+app.all('*', (req, res) => {
+  res.status(404).send(Boom.notFound('No route matched with those values', {}));
+});
+
+app.listen(port, () => {
+  CommonHelper.log(['Info'], `Server started on port ${port}`);
+});